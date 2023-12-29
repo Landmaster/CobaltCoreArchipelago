2ceb8e4f9f761c307b19460ea57e339aa16a09aa
Landmaster <python0429@gmail.com>
2023-12-28 20:49:44 -0500

    cursor/tooltip shows properly for archi

--- CCArchiManifest.cs
+++ CCArchiManifest.cs
@@ -4,6 +4,7 @@ using CobaltCoreModding.Definitions.ModContactPoints;
 using CobaltCoreModding.Definitions.ModManifests;
 using HarmonyLib;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace CobaltCoreArchipelago
 {
@@ -22,6 +23,10 @@ namespace CobaltCoreArchipelago
             //Logger?.LogWarning("Ow! But my face, though!");
             var harmony = new Harmony("Landmaster.CobaltCoreArchipelago.Patch");
             harmony.PatchAll();
+
+            var cornersField = typeof(G).GetField("corners", BindingFlags.Static | BindingFlags.NonPublic);
+            var cornersFieldVal = cornersField!.GetValue(null) as HashSet<UK>;
+            cornersFieldVal!.Add((UK)49201);
         }
 
         public void LoadManifest(ISpriteRegistry artRegistry)
@@ -37,10 +42,25 @@ namespace CobaltCoreArchipelago
         }
     }
 
+    class MouseDownHandler : global::OnMouseDown {
+
+        public void OnMouseDown(G g, Box b)
+        {
+        }
+    }
+
     [HarmonyPatch(typeof(CornerMenu), nameof(CornerMenu.Render))]
     class CornerMenuPatch {
         static void Postfix(G g, State s) {
-            Draw.Sprite((Spr)CCArchiSprites.ArchiIcon!.Id!, 1, 20);
+            var rect = new Rect(1, 20, 15, 15);
+            var box = g.Push(key: (UK)49201, rect: rect, onMouseDown: new MouseDownHandler());
+            if (box.IsHover()) {
+                g.tooltips.AddText(box.rect.xy + new Vec(15.0, 15.0), "Archipelago");
+            }
+            Draw.Sprite((Spr)CCArchiSprites.ArchiIcon!.Id!, rect.x, rect.y);
+            g.Pop();
         }
+
+        
     }
 }
