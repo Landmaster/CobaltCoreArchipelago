de1b546ac8509014eba437ae03456b846d324cf4
Landmaster <python0429@gmail.com>
2023-12-29 15:04:27 -0500

    replace play text w/ archi

---
+++ LocPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace CobaltCoreArchipelago
+{
+    [HarmonyPatch(typeof(Loc), nameof(Loc.T), new Type[] { typeof(string), typeof(string) })]
+    class LocPatch
+    {
+        static void Postfix(ref string __result, string key, string _) {
+            if (key.Equals("mainMenu.play") || key.Equals("mainMenu.continue")) {
+                __result = "Archipelago";
+            }
+        }
+    }
+}
