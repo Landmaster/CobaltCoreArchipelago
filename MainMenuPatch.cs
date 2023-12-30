e302c92f291398c6362e92cda34b6b6e181c00dd
Landmaster <python0429@gmail.com>
2023-12-30 14:14:21 -0500

    seed archi

--- MainMenuPatch.cs
+++ MainMenuPatch.cs
@@ -9,7 +9,7 @@ namespace CobaltCoreArchipelago
         [HarmonyPatch(nameof(MainMenu.OnMouseDown))]
         [HarmonyPostfix]
         static void OnMouseDownPostfix(G g, Box b) {
-            if (b.key == UK.menu_play) {
+            if (b.key == UK.menu_play && g.state.route.GetType() == typeof(NewRunOptions)) {
                 var slotData = CCArchiData.SlotData!;
 
                 g.state.PopulateRun(
@@ -20,7 +20,7 @@ namespace CobaltCoreArchipelago
                         (Deck) (int) (long) slotData["character3"]
                     },
                     difficulty: (int) (long) slotData["difficulty"],
-                    seed: 5908243,
+                    seed: (uint) slotData["seed"].GetHashCode(),
                     giveRunStartRewards: true
                 );
 
