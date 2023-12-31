e9707263f3d6b16b81dd7e94f69c3ccadddfd39b
Landmaster <python0429@gmail.com>
2023-12-30 19:32:38 -0500

    add card draw buttons

--- ArchiUKs.cs
+++ ArchiUKs.cs
@@ -3,5 +3,9 @@
     public class ArchiUKs
     {
         public const UK ArchiButton = (UK)49201;
+
+        public static UK CardRewardUK(int index) { 
+            return (UK)(70590 + index);
+        }
     }
 }
--- CCArchiRoute.cs
+++ CCArchiRoute.cs
@@ -7,6 +7,15 @@
             SharedArt.DrawEngineering(g);
 
             Draw.Text("Archipelago", 240, 24, font: DB.stapler, color: Colors.textMain, align: daisyowl.text.TAlign.Center);
+
+            var width = 120;
+            g.Push(rect: new Rect(120 - width / 2.0, 50));
+            for (int i = 0; i < 10; ++i) {
+                SharedArt.MenuItem(g, new Vec(0, 0 + 21 * i), width, false, ArchiUKs.CardRewardUK(i), "Card Draw " + (i+1));
+            }
+            
+            g.Pop();
+
             // TODO add more stuff
         }
     }
