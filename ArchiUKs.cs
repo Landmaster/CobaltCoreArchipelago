375802975146c52fb59ddd21146dffbec3d531ca
Landmaster <python0429@gmail.com>
2023-12-30 19:44:29 -0500

    more stuff to archi route

--- ArchiUKs.cs
+++ ArchiUKs.cs
@@ -5,7 +5,16 @@
         public const UK ArchiButton = (UK)49201;
 
         public static UK CardRewardUK(int index) { 
-            return (UK)(70590 + index);
+            return (UK)(70600 + index);
         }
+
+        public static UK ArtifactUK(int index) { 
+            return (UK)(70700 + index);
+        }
+
+        public const UK RareCardReward1UK = (UK)70800;
+        public const UK RareCardReward2UK = (UK)70801;
+        public const UK BossArtifact1UK = (UK)70900;
+        public const UK BossArtifact2UK = (UK)70901;
     }
 }
--- CCArchiRoute.cs
+++ CCArchiRoute.cs
@@ -9,11 +9,18 @@
             Draw.Text("Archipelago", 240, 24, font: DB.stapler, color: Colors.textMain, align: daisyowl.text.TAlign.Center);
 
             var width = 120;
+
             g.Push(rect: new Rect(120 - width / 2.0, 50));
             for (int i = 0; i < 10; ++i) {
                 SharedArt.MenuItem(g, new Vec(0, 0 + 21 * i), width, false, ArchiUKs.CardRewardUK(i), "Card Draw " + (i+1));
             }
-            
+            g.Pop();
+
+            g.Push(rect: new Rect(240 - width / 2.0, 50));
+            for (int i = 0; i<7; ++i)
+            {
+                SharedArt.MenuItem(g, new Vec(0, 0 + 21 * i), width, false, ArchiUKs.ArtifactUK(i), "Artifact " + (i + 1));
+            }
             g.Pop();
 
             // TODO add more stuff
